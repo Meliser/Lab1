6702a6059745f17bb5dfa53d27d22d444c64c731
Meliser <mitya.korneev@mail.ru>
2020-04-27 18:13:01 +0300

    Add 3 Jest tests
    Добавление 3 тестов Jest

--- src/components/ProfileInformation.vue
+++ src/components/ProfileInformation.vue
@@ -1,7 +1,7 @@
 <template>
 <b-container id="CProfileInformation" fluid class="d-flex flex-column
   align-items-center pt-5">
-  <b-container fluid class="d-flex align-items-center flex-column"
+  <b-container fluid id="user-info" class="d-flex align-items-center flex-column"
                v-if="!load_error && user_id.length > 0">
     <div v-if="user_avatar" class="mb-2">
       <b-avatar :src="user_avatar" size="15rem"></b-avatar>
@@ -14,12 +14,14 @@
     <p v-else><i>У пользователя нет статуса</i></p>
     <p v-if="user_online"><i>Пользователь сейчас онлайн!</i></p>
   </b-container>
-  <div v-else>
-    <b-alert show variant="danger" v-if="load_error">Пользователя не существует.</b-alert>
+  <div v-else id="user-not-found">
+    <b-alert show variant="danger" v-if="load_error">
+      Пользователя не существует.
+    </b-alert>
   </div>
   <b-row class="w-100" align-h="center">
     <b-col sm="12" md="8" lg="6">
-      <b-form-input fluid v-model="user_id"
+      <b-form-input id="user-input" fluid v-model="user_id"
                     placeholder="Введите ID или короткое имя пользователя"/>
     </b-col>
   </b-row>
@@ -43,35 +45,36 @@ export default {
       user_last_name: '',
     };
   },
-  computed: {
+  methods: {
+    async loadUser(userID) {
+      let userResponse = {};
+      await fetchJsonp(`https://api.vk.com/method/users.get?lang=0&user_ids=${userID}&fields=has_photo,photo_max_orig,online,status&access_token=${process.env.VUE_APP_VK_KEY}&v=5.103`)
+        .then((response) => response.json()).then((json) => {
+          userResponse = json;
+        });
+
+      if (userResponse.error || !(Object.keys(userResponse).length > 0)) {
+        this.user_avatar = null;
+        this.user_status = null;
+        this.load_error = true;
+      } else {
+        this.load_error = false;
+
+        const user = userResponse.response[0];
+        if (user.has_photo === 1) {
+          this.user_avatar = user.photo_max_orig;
+        } else {
+          this.user_avatar = null;
+        }
+        this.user_status = user.status;
+        this.user_online = user.online === 1;
+        this.user_first_name = user.first_name;
+        this.user_last_name = user.last_name;
+      }
+    },
   },
   watch: {
-    user_id(userID) {
-      fetchJsonp(`https://api.vk.com/method/users.get?lang=0&user_ids=${userID}&fields=has_photo,photo_max_orig,online,status&access_token=${process.env.VUE_APP_VK_KEY}&v=5.103`)
-        .then((response) => response.json()).then((json) => {
-          if (json.error) {
-            this.user_avatar = null;
-            this.user_status = null;
-            this.load_error = true;
-          } else {
-            this.load_error = false;
-
-            const user = json.response[0];
-            if (user.has_photo === 1) {
-              this.user_avatar = user.photo_max_orig;
-            } else {
-              this.user_avatar = null;
-            }
-            this.user_status = user.status;
-            this.user_online = user.online === 1;
-            this.user_first_name = user.first_name;
-            this.user_last_name = user.last_name;
-          }
-        }).catch(() => {
-          this.user_avatar = null;
-          this.user_status = null;
-        });
-    },
+    async user_id(userID) { return this.loadUser(userID); },
   },
 };
 </script>
--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -2,11 +2,20 @@ import { shallowMount } from '@vue/test-utils';
 import ProfileInformation from '@/components/ProfileInformation.vue';
 
 describe('ProfileInformation.vue', () => {
-  it('renders props.msg when passed', () => {
-    const msg = 'new message';
-    const wrapper = shallowMount(ProfileInformation, {
-      propsData: { msg },
-    });
-    expect(wrapper.text()).toMatch(msg);
+  it('VK API key must persist in environment', async () => {
+    expect(!!process.env.VUE_APP_VK_KEY).toBeTruthy();
+  });
+
+  it('User id changes must trigger api request', async () => {
+    const wrapper = shallowMount(ProfileInformation);
+    wrapper.setData({ user_id: 'deerose0' });
+    await wrapper.vm.$nextTick();
+    expect(wrapper.contains('#user-info')).toBeTruthy();
+  });
+
+  it('Wrong request must show error message', async () => {
+    const wrapper = shallowMount(ProfileInformation);
+    wrapper.setData({ user_id: '--- unbelievable user name ---' });
+    expect(wrapper.contains('#user-not-found')).toBeTruthy();
   });
 });
